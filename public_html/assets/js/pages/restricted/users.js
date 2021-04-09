f1a368a90e1ac161fb8e081bd79867142f87b832
Roger Eliodoro Condras <32995553+RogerEC@users.noreply.github.com>
2021-04-08 23:12:00 -0300

    Adicionada confirmação antes de excluir o link e acrescentado navbar nas páginas restritas

--- public_html/assets/js/pages/restricted/users.js
+++ public_html/assets/js/pages/restricted/users.js
@@ -6,8 +6,11 @@ $(document).ready(function () {
         event.preventDefault();
         // troca o botão de exibir para salvar
         $(".editButton").attr("disabled", true);
+        $(".buttonDelete").attr("disabled", true);
         $(this).hide();
         $("#saveButton"+$(this).val()).attr("hidden", false);
+        $("#buttonDelete"+$(this).val()).hide();
+        $("#buttonCancel"+$(this).val()).attr("hidden", false);
         // habilita campos para edição
         $("#name"+$(this).val()).attr("disabled", false);
         $("#status"+$(this).val()).attr("disabled", false);
@@ -36,8 +39,11 @@ $(document).ready(function () {
                             event.preventDefault();
                             // troca botão salvar para editar e ativa a edição dos demais
                             $(".editButton").attr("disabled", false);
+                            $(".buttonDelete").attr("disabled", false);
                             $(this).attr("hidden", true);
                             $("#editButton"+$(this).val()).show();
+                            $("#buttonDelete"+$(this).val()).show();
+                            $("#buttonCancel"+$(this).val()).attr("hidden", true);
                             // desabilita campos para edição
                             $("#name"+$(this).val()).attr("disabled", true);
                             $("#status"+$(this).val()).attr("disabled", true);
@@ -88,10 +94,40 @@ $(document).ready(function () {
 
     $(".buttonDelete").on("click", function(event){
         event.preventDefault();
+        $("#buttonDeleteConfirmation").val($("#idLinks"+$(this).val()).val());
+        $("#modalDeleteConfirmation").click();
+        return;
+        
+    });
+
+    $(".buttonCancel").on("click", function(event){
+        event.preventDefault();
+        // restaura os valores originais, caso alterados
+        $("#name"+$(this).val()).val(link.name);
+        $("#status"+$(this).val()).prop('checked', link.status);
+        $("#permanentLink"+$(this).val()).prop('checked', link.permanentLink);
+        $("#url"+$(this).val()).val(link.url);
+        $("#expirationDatetime"+$(this).val()).val(link.expirationDatetime);
+        $(".editButton").attr("disabled", false);
+        $(".buttonDelete").attr("disabled", false);
+        $(this).attr("hidden", true);
+        $("#editButton"+$(this).val()).show();
+        $("#buttonDelete"+$(this).val()).show();
+        $("#saveButton"+$(this).val()).attr("hidden", true);
+        // desabilita campos para edição
+        $("#name"+$(this).val()).attr("disabled", true);
+        $("#status"+$(this).val()).attr("disabled", true);
+        $("#permanentLink"+$(this).val()).attr("disabled", true);
+        $("#url"+$(this).val()).attr("disabled", true);
+        $("#expirationDatetime"+$(this).val()).attr("disabled", true);
+    });
+
+    $("#buttonDeleteConfirmation").on("click", function(event){
+        event.preventDefault();
         $(".editButton").attr("disabled", true);
         $(".buttonDelete").attr("disabled", true);
         $("#buttonAddLink").attr("disabled", true);
-        $.post('/usuario/gestor/links/delete', { idLink: $("#idLinks"+$(this).val()).val()}).done(function(){
+        $.post('/usuario/gestor/links/delete', { idLink: $(this).val()}).done(function(){
             window.location.reload();
         }).fail(function(){
             alert("Erro ao tentar realizar a exclusão.");
@@ -99,5 +135,5 @@ $(document).ready(function () {
             $(".buttonDelete").attr("disabled", false);
             $("#buttonAddLink").attr("disabled", false);
         });
-    })
+    });
 });
